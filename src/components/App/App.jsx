e55fa545bfcbe8ced7ed7baccd19a6c7ed0e6a5a
Tre Olson <treolson@live.com>
2021-11-18 09:52:30 -0600

    added css

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LaunchIcon from '@material-ui/icons/Launch';
 import './App.css';
 const {
     main
@@ -15,45 +17,46 @@ function App () {
     return (
         <>
         <div className="App">
-            <h1>Heat Transfer Warehouse Admin Portal</h1>
+            <h1 className="headerName">Heat Transfer Warehouse Admin Portal</h1>
         </div>
         <section>
             <div className="container">
                 <h3>Affiliate App</h3>
                 <ul>
-                    <li><a href="https://affiliates.heattransferwarehouse.com/" target="_blank">Open App</a></li>
-                    <li><a href="https://github.com/cneisen234/affiliate_orders" target="_blank">Github</a></li>
+                    <li><a href="https://affiliates.heattransferwarehouse.com/" target="_blank"><LaunchIcon/></a></li>
+                    <li><a href="https://github.com/cneisen234/affiliate_orders" target="_blank"><GitHubIcon/></a></li>
                 </ul>
             </div>
             <div className="container">
                 <h3>Decoqueue</h3>
                 <ul>
-                    <li><a href="https://decoqueue.heattransferwarehouse.com/" target="_blank">Open App</a></li>
-                    <li><a href="https://github.com/cneisen234/decovibe_queue" target="_blank">Github</a></li>
+                    <li><a href="https://decoqueue.heattransferwarehouse.com/" target="_blank"><LaunchIcon/></a></li>
+                    <li><a href="https://github.com/cneisen234/decovibe_queue" target="_blank"><GitHubIcon/></a></li>
                 </ul>
             </div>
             <div className="container">
                 <h3>Order Calculator</h3>
                 <ul>
-                    <li><a href="http://orderapp.heattransferwarehouse.com/" target="_blank">Open App</a></li>
-                    <li><a href="https://github.com/cneisen234/order_calc" target="_blank">Github</a></li>
+                    <li><a href="http://orderapp.heattransferwarehouse.com/" target="_blank"><LaunchIcon/></a></li>
+                    <li><a href="https://github.com/cneisen234/order_calc" target="_blank"><GitHubIcon/></a></li>
                 </ul>
             </div>
             <div className="container">
                 <h3>No Stock Notify</h3>
                 <ul>
-                    <li><a href="https://nostock.heattransferwarehouse.com/" target="_blank">Open App</a></li>
-                    <li><a href="https://github.com/Traizel/HTW-No-Stock-Nofify" target="_blank">Github</a></li>
+                    <li><a href="https://nostock.heattransferwarehouse.com/" target="_blank"><LaunchIcon/></a></li>
+                    <li><a href="https://github.com/Traizel/HTW-No-Stock-Nofify" target="_blank"><GitHubIcon/></a></li>
                 </ul>
             </div>
         </section>
-        <div>
-            <h3>Interval (Minutes)</h3>
-            <input type="number" value={interval} onChange={(e) => (setInterval(e.target.value))}/>
-            <br/>
-            <br/>
-            <button onClick={(e) => (BPOrderCapture())}>Manual Order Capture (BrightPearl)</button>
-        </div>
+        {//<div>
+         //   <h3>Interval (Minutes)</h3>
+         //   <input type="number" value={interval} onChange={(e) => (setInterval(e.target.value))}/>
+         //   <br/>
+         //   <br/>
+         //   <button onClick={(e) => (BPOrderCapture())}>Manual Order Capture (BrightPearl)</button>
+         // </div>
+        }
         </>
     )
 }
